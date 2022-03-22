d9a82dfcc96116202d147b430010ace6e1d07bd9
uglyonlytoday <superjang3357@naver.com>
2022-03-22 18:15:56 +0900

    add text_theme

---
+++ lib/core/theme/text_theme.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:booklog/core/theme/color_theme.dart';
+import 'package:flutter/material.dart';
+
+abstract class DPTextTheme {
+  static const BODY1 = TextStyle(fontFamily: 'Pretendard', fontWeight: FontWeight.normal, fontSize: 16, height: 1.5, color: BLColors.TEXT);
+  static const BODY2 = TextStyle(fontFamily: 'Pretendard', fontWeight: FontWeight.normal, fontSize: 14, height: 1.5, color: BLColors.TEXT87);
+}
