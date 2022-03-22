cdbae7e608d21ca98a49cea6e5e8d0271faa12fb
uglyonlytoday <superjang3357@naver.com>
2022-03-22 18:27:01 +0900

    rename DPTextTheme -> BLTextTheme

--- lib/core/theme/text_theme.dart
+++ lib/core/theme/text_theme.dart
@@ -3,7 +3,7 @@
 import 'package:booklog/core/theme/color_theme.dart';
 import 'package:flutter/material.dart';
 
-abstract class DPTextTheme {
+abstract class BLTextTheme {
   static const BODY1 = TextStyle(fontFamily: 'Pretendard', fontWeight: FontWeight.normal, fontSize: 16, height: 1.5, color: BLColors.TEXT);
   static const BODY2 = TextStyle(fontFamily: 'Pretendard', fontWeight: FontWeight.normal, fontSize: 14, height: 1.5, color: BLColors.TEXT87);
 }
