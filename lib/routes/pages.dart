880f22f544840711a1cdf9dcecf4c90d48b7aff0
uglyonlytoday <superjang3357@naver.com>
2022-03-22 17:32:57 +0900

    add TestPage route

--- lib/routes/pages.dart
+++ lib/routes/pages.dart
@@ -1,6 +1,9 @@
+import 'package:booklog/app/modules/test/page.dart';
 import 'package:get/get.dart';
 part './routes.dart';
 
 abstract class AppPages {
-  static final pages = <GetPage>[];
+  static final pages = <GetPage>[
+    GetPage(name: Routes.TEST, page: () => const TestPage()),
+  ];
 }
