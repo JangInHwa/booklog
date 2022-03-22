5cf1162d09ac1bf8c7328a1a7b87329570255038
uglyonlytoday <superjang3357@naver.com>
2022-03-22 18:13:34 +0900

    add color_theme

---
+++ lib/core/theme/color_theme.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:flutter/material.dart';
+
+abstract class BLColors {
+  static const DARK1 = Color(0xFF111111);
+  static const DARK2 = Color(0xFF202020);
+  static const DARK3 = Color(0xFF333333);
+  static const TEXT = Color(0xFFFFFFFF);
+  static const TEXT87 = Color.fromRGBO(255, 255, 255, 0.87);
+}
