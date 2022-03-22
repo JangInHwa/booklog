70d2d2e1e5707260c60b035578aa7efb9de544f6
uglyonlytoday <superjang3357@naver.com>
2022-03-22 22:25:39 +0900

    edit BookSearchResult model

--- lib/app/data/models/book_search_result.dart
+++ lib/app/data/models/book_search_result.dart
@@ -1,5 +1,12 @@
 class BookSearchResult {
   String title;
   String author;
-  BookSearchResult({required this.title, required this.author});
+  String thumbnailPath;
+  BookSearchResult({required this.title, required this.author, required this.thumbnailPath});
+
+  factory BookSearchResult.fromJson(Map<String, dynamic> json) => BookSearchResult(
+        title: json['title'],
+        author: json['author'],
+        thumbnailPath: json['image'],
+      );
 }
