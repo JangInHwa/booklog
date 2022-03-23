e89e65d455d89f599e024095fb28b0ac4164850d
uglyonlytoday <superjang3357@naver.com>
2022-03-23 20:44:03 +0900

    add HEADER1, HEADER2 TextTheme

--- lib/core/theme/text_theme.dart
+++ lib/core/theme/text_theme.dart
@@ -4,6 +4,8 @@ import 'package:booklog/core/theme/color_theme.dart';
 import 'package:flutter/material.dart';
 
 abstract class BLTextTheme {
+  static const HEADER1 = TextStyle(fontFamily: 'Pretendard', fontWeight: FontWeight.bold, fontSize: 28, height: 1.2, color: BLColors.TEXT);
+  static const HEADER2 = TextStyle(fontFamily: 'Pretendard', fontWeight: FontWeight.bold, fontSize: 22, height: 1.2, color: BLColors.TEXT);
   static const HEADER3 = TextStyle(fontFamily: 'Pretendard', fontWeight: FontWeight.bold, fontSize: 20, height: 1.2, color: BLColors.TEXT);
   static const BODY1 = TextStyle(fontFamily: 'Pretendard', fontWeight: FontWeight.normal, fontSize: 16, height: 1.5, color: BLColors.TEXT);
   static const BODY2 = TextStyle(fontFamily: 'Pretendard', fontWeight: FontWeight.normal, fontSize: 14, height: 1.5, color: BLColors.TEXT);
